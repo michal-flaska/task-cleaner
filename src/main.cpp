5b29fe3e59b9b78c2a54fb0d6d348f4173ee73b7
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-10-05 10:01:36 +0200

    add auto-registration (auto run on startup)

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,35 @@ void HideConsoleWindow() {
 	ShowWindow(hwnd, SW_HIDE);
 }
 
+bool IsRegisteredInStartup() {
+	HKEY hKey;
+	if (RegOpenKeyExA(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+		char value[MAX_PATH];
+		DWORD size = sizeof(value);
+		bool exists = RegQueryValueExA(hKey, "TaskCleaner", NULL, NULL, (LPBYTE)value, &size) == ERROR_SUCCESS;
+		RegCloseKey(hKey);
+		return exists;
+	}
+	return false;
+}
+
+void RegisterStartup() {
+	char exePath[MAX_PATH];
+	GetModuleFileNameA(NULL, exePath, MAX_PATH);
+
+	HKEY hKey;
+	if (RegOpenKeyExA(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE, &hKey) == ERROR_SUCCESS) {
+		RegSetValueExA(hKey, "TaskCleaner", 0, REG_SZ, (LPBYTE)exePath, (DWORD)strlen(exePath) + 1);
+		RegCloseKey(hKey);
+	}
+}
+
 int main() {
 	HideConsoleWindow();
+
+	if (!IsRegisteredInStartup()) {
+		RegisterStartup();
+	}
 
 	try {
 		Config config = ConfigLoader::Load("config.json");
